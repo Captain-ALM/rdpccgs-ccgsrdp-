05e1ea102d1d7958bbf25ebb5e9bce59c3e54c14
Captain ALM <alm00000@outlook.com>
2021-05-28 18:15:58 +0100

    Update restart lib module name.

--- restartsblib/restartsblib/main.vb
+++ restartsblib/restartsblib/main.vb
@@ -5,7 +5,7 @@ Imports System.Reflection
 ''' </summary>
 ''' <remarks></remarks>
 <SmallBasicType()>
-Public Module mainrs
+Public Module SBRelaunchExtension
     ''' <summary>
     ''' Attempts to start the program with the specified arguments
     ''' Program.End() still needs to be called after!
@@ -13,7 +13,7 @@ Public Module mainrs
     ''' <param name="arguments">The arguments to pass to the started application</param>
     ''' <returns>The session ID of the started application</returns>
     ''' <remarks></remarks>
-    Public Function restartprogram(arguments As Primitive) As Primitive
+    Public Function relaunchprogram(arguments As Primitive) As Primitive
         Dim hostapp As String = Assembly.GetEntryAssembly.Location
         If arguments.ToString <> "" Then
             Return New Primitive(Process.Start(hostapp, arguments.ToString).SessionId)
@@ -28,12 +28,20 @@ Public Module mainrs
     ''' <param name="arguments">The arguments to pass to the started application</param>
     ''' <returns>The session ID of the started application</returns>
     ''' <remarks></remarks>
-    Public Function restartprogramasadmin(arguments As Primitive) As Primitive
+    Public Function relaunchprogramasadmin(arguments As Primitive) As Primitive
         Dim hostapp As String = Assembly.GetEntryAssembly.Location
         If arguments.ToString <> "" Then
             Return New Primitive(Process.Start(New ProcessStartInfo() With {.FileName = hostapp, .Arguments = arguments.ToString, .Verb = "runas"}).SessionId)
         Else
             Return New Primitive("")
         End If
+    End Function
+    ''' <summary>
+    ''' Gets the path of the executing assembly
+    ''' </summary>
+    ''' <returns>The path of the executing assembly</returns>
+    ''' <remarks></remarks>
+    Public Function programPath() As Primitive
+        Return Assembly.GetEntryAssembly.Location
     End Function
 End Module
